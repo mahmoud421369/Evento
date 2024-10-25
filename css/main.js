4eeaccc9991336d6f9e2e4dd539a66c072cbd53a
mahmoud421369 <mahmoudakskfjdk@gmail.com>
2024-10-25 17:44:16 +0300

    Add files via upload

---
+++ css/main.js
@@ -0,0 +1,24 @@
+// let imgs = document.querySelectorAll('.slider img');
+// let dots = document.querySelectorAll(".dot");
+// let currentImg = 0; 
+// const interval = 3000; 
+
+// function changeSlide(n){
+//     for(let i = 0;i< imgs.length;i++){
+//         imgs[i].style.opacity = 0;
+//         dots[i].className = dots[i].className.replace('active','');
+//     }
+//     currentImg = n;
+//     imgs[currentImg].style.opacity = 1;
+//     dots[currentImg].className += "active";
+// }
+// let timer = setInterval(changeSlide,interval);
+
+// currentImg = (currentImg + 1 ) % imgs.length;
+// if(n != undefined){
+//     clearInterval(timer);
+//     timer = setInterval(changeSlide,interval);
+//     currentImg = n;
+// }
+
+
